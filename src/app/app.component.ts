15aa706d95831290c5d4f6fa855df6c1ccfa959f
Rakesh Yadav <rky8966@gmail.com>
2025-01-19 14:26:52 +0530

    Enhanced: added ngrx effects store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:[CounterOutputComponent,CounterControlsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideEffects } from '@ngrx/effects';
+import { CounterEffects } from './store/counter.effects';
+import { counterReducer } from './store/counter.reducer';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideEffects([CounterEffects]),provideStore({ counter: counterReducer })]
 };
